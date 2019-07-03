832ae69f05052afceffe87dbdc29cddd6653bbf2
nightfall-w <rimuli_w@163.com>
2019-07-03 23:25:06 +0800

    index serializer

--- info/serializer.py
+++ info/serializer.py
@@ -2,5 +2,14 @@
 
 from rest_framework import serializers
 
-class IndexSerializer(serializers.Serializer):
-    pass
+
+class schemeSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=20, required=True)
+    day = serializers.IntegerField(required=True)
+    night = serializers.IntegerField(required=True)
+    beLike = serializers.BooleanField(default=False, required=True)
+    scenic = serializers.FloatField(default=4.0, required=True)
+    photoUrl = serializers.CharField(max_length=200, required=True)
+    reviewNum = serializers.IntegerField(required=True)
+    unit_price = serializers.IntegerField(required=True)
+
