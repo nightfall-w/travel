ef3d95a8e3655e25dd2521bc8148d284c8b78adb
baojunw <rimuli_w@163.com>
2019-07-04 16:54:47 +0800

    'index——page——view'

--- info/views.py
+++ info/views.py
@@ -1,14 +1,33 @@
+import datetime
 from django.shortcuts import render
 from rest_framework.views import APIView
+from info.models import Scheme, Review
+from info.serializer import schemeSerializer
 from rest_framework.response import Response
 from django.utils.decorators import method_decorator
-
+from django.db.models import Count
 
 # Create your views here.
 
 class Index(APIView):
     def get(self, request):
+        # ???????? ???
+        end_locales = set(Scheme.objects.all().values_list('end_locale', flat=True))
 
+        # ???????????????
+        years = [datetime.datetime.now().year, datetime.datetime.now().year + 1]
+
+        # ????????????????????
+        today = datetime.date.today()
+        to_date = today + datetime.timedelta(days=1)
+        week_ago = today - datetime.timedelta(days=7)
+        month_age = today - datetime.timedelta(days=30)
+        # ????????(????? ?????)
+        week_hot_review = Review.objects.filter(create__range=(week_ago, to_date))
+        if not week_hot_review:
+            week_hot_review = Review.objects.filter(create__range=(week_ago, to_date))
+        ss = week_hot_review.only('scheme').order_by("scheme")
+        # schemeSerializer()
         return render(request, 'index.html')
 
 
