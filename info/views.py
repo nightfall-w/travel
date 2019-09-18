a905707b264eb2600e69451998d868849b0e2bfa
nightfall-w <rimuli_w@163.com>
2019-09-18 15:25:22 +0800

    Mock数据以及评分逻辑实现并渲染

--- info/views.py
+++ info/views.py
@@ -40,17 +40,68 @@ class Detail(APIView):
     def get(self, request, *args, **kwargs):
         id = kwargs.get('id')
         scheme = Scheme.objects.filter(_id=id).first()
+
+        # 为找到scheme 返回404页面
         if not scheme:
             return render(request, '404.html')
+
+        # 统计游览景点个数
         scenic_number = 0
         for journey in scheme.journeys:
-            for scenic in journey.visit_address:
-                scenic_number += 1
+            scenic_number += len(journey.visit_address)
         scheme.scenic_number = scenic_number
 
+        # 返回3个相似陶套餐(目的地相同)
         similar_schemes = Scheme.objects.filter(Q(destination=scheme.destination) & Q(_id__ne=scheme._id))[0:3]
 
-        return render(request, 'detail-page.html', {'scheme': scheme, 'similar_schemes': similar_schemes})
+        # 返回评分信息
+        if 4.5 <= scheme.avg_score <= 5:
+            level = "极好"
+        elif 4 <= scheme.avg_score < 4.5:
+            level = "优秀"
+        elif 3 <= scheme.avg_score < 4:
+            level = "良好"
+        else:
+            level = "一般"
+
+
+        """
+            Mock 数据
+        """
+        # 评分排名
+        all_scheme_score = [scheme.avg_score for scheme in Scheme.objects.all()]
+        all_scheme_score.sort()
+        all_scheme_score = all_scheme_score[::-1]
+        all_scheme_len = len(all_scheme_score)
+        index = all_scheme_score.index(scheme.avg_score, -1)
+        score_ranking = "%.2f%%" % (index / all_scheme_len * 100)
+
+        # 清洁分
+        cleanliness = 3.4
+        cleanliness_score_count = 133
+        cleanliness_percentage = "%.2f" % (cleanliness / 5 * 100)
+        # 服务
+        service = 4.6
+        service_score_count = 133
+        service_percentage = "%.2f" % (service / 5 * 100)
+        # 舒适
+        comfortable = 4.2
+        comfortable_score_count = 133
+        comfortable_percentage = "%.2f" % (comfortable / 5 * 100)
+        # 条件
+        condition = 4.7
+        condition_score_count = 133
+        condition_percentage = "%.2f" % (condition / 5 * 100)
+
+        return render(request, 'detail-page.html',
+                      {'scheme': scheme, 'similar_schemes': similar_schemes, 'avg_score': scheme.avg_score,
+                       'level': level, 'score_ranking': score_ranking, 'cleanliness': cleanliness,
+                       'cleanliness_percentage': cleanliness_percentage, 'service': service,
+                       'service_percentage': service_percentage, 'comfortable': comfortable,
+                       'comfortable_percentage': comfortable_percentage, 'condition': condition,
+                       'condition_percentage': condition_percentage, 'cleanliness_score_count': cleanliness_score_count,
+                       'service_score_count': service_score_count, 'comfortable_score_count': comfortable_score_count,
+                       'condition_score_count': condition_score_count})
 
 
 class Schemes(APIView):
@@ -75,7 +126,7 @@ class Schemes(APIView):
 
 class ScenicSpot(APIView):
     '''
-    推荐热门目的地
+    推荐受关注的目的地
     '''
 
     def get(self, request):
